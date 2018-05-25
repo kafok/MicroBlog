8d478c5d52d286c49de7a7abfdca992164c158fd
Jose Antonio <toteromero91.toteromero91@gmail.com>
2018-05-25 12:46:31 +0200

    Añadido script de registros de la base de datos.

--- misc/sql/populate.sql
+++ misc/sql/populate.sql
@@ -7,6 +7,6 @@ INSERT INTO `user` (`id_u`, `Email`, `Password`, `Name`, `Profile`) VALUES
 (2, 'cristina@gmail.com', '123456789', 'Cristina ', ''),
 (3, 'joseantonio@gmail.com', '123456789', 'Joseantonio', '');
 
-INSERT INTO `news` (`id_n`, `Fecha`, `titulo`, `descripcion`) VALUES
-(1, '2018-05-25 13:00:00', 'Final de la UEFA Champions League 2018', 'El próximo sábado 26 de Mayo se celebra la final de la UEFA Champions Leaque entre el Real Madrid y el Liverpool en la Ciudad de Kiev.'),
-(2, '2018-05-25 14:00:00', 'PSOE presenta Moción de Censura', 'El secretario general del PSOE Pedro Sánchez quiere presentar una moción de censura al partido político del PP.');
+INSERT INTO `news` (`id_n`, `Fecha`, `titulo`, `descripcion`,`userId`) VALUES
+(1, '2018-05-25 13:00:00', 'Final de la UEFA Champions League 2018', 'El próximo sábado 26 de Mayo se celebra la final de la UEFA Champions Leaque entre el Real Madrid y el Liverpool en la Ciudad de Kiev.',3),
+(2, '2018-05-25 14:00:00', 'PSOE presenta Moción de Censura', 'El secretario general del PSOE Pedro Sánchez quiere presentar una moción de censura al partido político del PP.',3);
