04c14cd1fb8d96c3d7aea0c4ec0ae145f8cff9cd
crileaech <crileaech@gmail.com>
2018-05-25 12:52:22 +0200

    Completado get de repositorios

--- back-end/src/com/avante/repositories/NewsRepository.java
+++ back-end/src/com/avante/repositories/NewsRepository.java
@@ -1,4 +1,13 @@
 package com.avante.repositories;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.avante.servlet.news.*;
+
+import com.avante.DatabaseConnectionFactory;
+import com.avante.model.News;
 
 public class NewsRepository {
 
@@ -15,6 +24,41 @@ public class NewsRepository {
 		super();
 	}
 	
+	public News get(int id) throws SQLException{
+		//get connection from connection pool
+		Connection con = DatabaseConnectionFactory.getConnectionFactory().getConnection();
+		try {
+			final String sql = "select * from microblog.news where id = " +id;
+			//create prepared statement with option to get auto generated keys
+			PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.execute();
+			//Get auto generated keys
+	        ResultSet rs = stmt.getGeneratedKeys(); 
+	        News n = new News();
+	        
+	        while (rs.next()) {
+	        	n.setId(rs.getInt("id_n"));
+	        	n.setFecha(rs.getDate("fecha"));
+	            n.setTitulo(rs.getString("titulo"));
+	            n.setDescripcion(rs.getString("descripcion"));;
+	            n.setUserId(rs.getInt("userId"));
+	        }
+	        
+	        rs.close();
+	        stmt.close();
+	        return n;
+		} catch (SQLException e) {
+	        throw new IllegalStateException(e);}
+		finally {
+			con.close();
+		}
+	}
+//	public News insert() throws SQLException{
+//		//get connection from connection pool
+//		Connection con = DatabaseConnectionFactory.getConnectionFactory().getConnection();
+//		try
+//	}
 	
 	
 }
+
--- back-end/src/com/avante/repositories/UserRepository.java
+++ back-end/src/com/avante/repositories/UserRepository.java
@@ -1,5 +1,14 @@
 package com.avante.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.avante.DatabaseConnectionFactory;
+import com.avante.model.User;
+
 public class UserRepository {
 
 	private static UserRepository instance = null;
@@ -15,6 +24,35 @@ public class UserRepository {
 		super();
 	}
 	
+	public User get(int id) throws SQLException{
+		//get connection from connection pool
+		Connection con = DatabaseConnectionFactory.getConnectionFactory().getConnection();
+		try {
+			final String sql = "select * from microblog.news where id = " +id;
+			//create prepared statement with option to get auto generated keys
+			PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.execute();
+			//Get auto generated keys
+	        ResultSet rs = stmt.getGeneratedKeys(); 
+	        User u = new User();
+	        
+	        while (rs.next()) {
+	        	u.setId(rs.getInt("id_u"));
+	        	u.setEmail(rs.getString("email"));
+	            u.setPassword(rs.getString("password"));
+	            u.setProfile(rs.getString("profile"));;
+	            
+	        }
+	        
+	        rs.close();
+	        stmt.close();
+	        return u;
+		} catch (SQLException e) {
+	        throw new IllegalStateException(e);}
+		finally {
+			con.close();
+		}
+	}
 	
 	
 }
