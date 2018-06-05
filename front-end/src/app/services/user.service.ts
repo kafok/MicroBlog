740dba2e627646e8e557f96dbf9f69b4f184596e
Marcos <marcos.kafok@gmail.com>
2018-06-05 12:05:57 +0200

    Completado servicio de usuario

--- front-end/src/app/services/user.service.ts
+++ front-end/src/app/services/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { Login } from '../models/login';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  public principal: User;
+
+
+  constructor(public http: HttpClient) { }
+
+
+  public login(login: Login): Observable<any> {
+    return this.http.post(environment.baseApi + "user/signin", login);
+  }
+
+  public logout(): Observable<any> {
+    return this.http.post(environment.baseApi + "user/signout", {});
+  }
+
+  public getUser(id: number) {
+    return this.http.get(environment.baseApi + "user/get?id=" + id);
+  }
+
+  public getPrincipal() {
+    return this.http.get(environment.baseApi + "user/principal");
+  }
 }
