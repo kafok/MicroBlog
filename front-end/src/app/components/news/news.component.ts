c27bce58a66d783a48c302bcfab9c8dcc14af0fd
Marcos <marcos.kafok@gmail.com>
2018-06-07 12:03:44 +0200

    Agregada la opción de ver la noticia

--- front-end/src/app/components/news/news.component.ts
+++ front-end/src/app/components/news/news.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../../services/news.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { News } from '../../models/news';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'news',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  public loaded: boolean;
+  public news: News;
+
+  constructor(private newsService: NewsService, 
+    private aroute: ActivatedRoute, 
+    private userService: UserService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.aroute.params.subscribe(params => {
+      this.newsService.getNews(params.id).subscribe(res => {
+        this.news = res;
+        this.loaded = true;
+      }, error => {
+        this.news = null;
+        this.loaded = true;
+      });
+    });
   }
 
+
+  public imOwner(): boolean {
+    if(this.userService.isAnonymous())
+      return false;
+
+    return this.userService.principal.id == this.news.userId;
+  }
 }
